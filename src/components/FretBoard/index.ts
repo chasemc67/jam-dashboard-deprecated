c6a9be4405a46b0eac29859bee36dc2174cdd8d3
= <chasemc67@gmai.com>
2023-03-31 19:27:12 -0700

    Added fretboard controls that let you pick strings

--- src/components/FretBoard/index.ts
+++ src/components/FretBoard/index.ts
@@ -1,1 +1,2 @@
 export { default } from './FretBoard';
+export type {FretBoardProps} from './FretBoard';
---
+++ src/components/FretboardControls/FretboardControls.tsx
@@ -0,0 +1,73 @@
+// FretboardControls.tsx
+import React, { FunctionComponent, useState } from 'react';
+import styled from 'styled-components';
+import FretBoard, { FretBoardProps } from '../FretBoard';
+
+const FretboardControlsContainer = styled.div<{ isLeftHanded: boolean }>`
+  display: flex;
+  flex-direction: ${(props) => (props.isLeftHanded ? 'row-reverse' : 'row')};
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const TextInput = styled.input`
+  width: 30px;
+  height: 30px;
+  margin-bottom: 10px;
+  text-align: center;
+`;
+
+const FretboardControls: FunctionComponent = ({
+}) => {
+  const [rootNotes, setRootNotes] = useState(['E', 'A', 'D', 'G', 'B', 'E']);
+
+  const highlightedNotes = [
+    { note: 'C', color: 'blue' },
+    { note: 'D', color: 'red' },
+    { note: 'E', color: 'green' },
+    { note: 'F', color: 'orange' },
+    { note: 'G', color: 'brown' },
+    { note: 'A', color: 'purple' },
+    { note: 'B', color: 'teal' },
+  ];
+  const numberOfFrets = 12;
+  const startingFret = 0;
+  const showTextNotes = true;
+  const isLeftHanded = false;
+
+  const handleInputChange = (index: number, value: string) => {
+    const updatedRootNotes = [...rootNotes];
+    updatedRootNotes[index] = value;
+    setRootNotes(updatedRootNotes);
+  };
+
+  const renderInputs = () => {
+    return rootNotes.map((note, index) => (
+      <TextInput
+        key={index}
+        value={note}
+        onChange={(e) => handleInputChange(index, e.target.value)}
+      />
+    ));
+  };
+
+  return (
+    <FretboardControlsContainer isLeftHanded={isLeftHanded}>
+      <InputContainer>{renderInputs()}</InputContainer>
+      <FretBoard
+        rootNotes={rootNotes}
+        highlightedNotes={highlightedNotes}
+        numberOfFrets={numberOfFrets}
+        startingFret={startingFret}
+        showTextNotes={showTextNotes}
+        isLeftHanded={isLeftHanded}
+      />
+    </FretboardControlsContainer>
+  );
+};
+
+export default FretboardControls;
