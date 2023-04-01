1050c5951b31d0070a96293941266413881bb1fa
= <chasemc67@gmai.com>
2023-03-31 21:56:32 -0700

    Its ready for V1 release

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import FretboardControls from './components/FretboardControls/FretboardControls';
+
+const AppBody = styled.div`
+  margin: 20px;
+`;
+
+const AppHeader = styled.header`
+  display: flex;
+  justify-content: center;
+`;
 
 function App() {
   return (
     <div className="App">
-      <FretboardControls />
+      <AppHeader>
+        <h1>Jam Dashboard</h1>
+      </AppHeader>
+      <AppBody>
+        <FretboardControls />
+      </AppBody>
     </div>
   );
 }
