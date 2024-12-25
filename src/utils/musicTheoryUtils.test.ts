b6f684151f8eeba2470876529fa2277734db1900
chase <chase@codegen.com>
2024-12-24 18:18:17 -0800

    added unit tests

---
+++ src/utils/musicTheoryUtils.test.ts
@@ -0,0 +1,42 @@
+import { areNotesEquivalent } from './musicTheoryUtils';
+
+describe('areNotesEquivalent', () => {
+    test('identifies equivalent natural notes', () => {
+        expect(areNotesEquivalent('C', 'C')).toBe(true);
+        expect(areNotesEquivalent('D', 'D')).toBe(true);
+        expect(areNotesEquivalent('C', 'D')).toBe(false);
+    });
+
+    test('identifies equivalent sharp and flat notes', () => {
+        expect(areNotesEquivalent('C#', 'Db')).toBe(true);
+        expect(areNotesEquivalent('D#', 'Eb')).toBe(true);
+        expect(areNotesEquivalent('F#', 'Gb')).toBe(true);
+        expect(areNotesEquivalent('G#', 'Ab')).toBe(true);
+        expect(areNotesEquivalent('A#', 'Bb')).toBe(true);
+    });
+
+    test('identifies equivalent double sharp and double flat notes', () => {
+        expect(areNotesEquivalent('C##', 'D')).toBe(true);
+        expect(areNotesEquivalent('Dbb', 'C')).toBe(true);
+        expect(areNotesEquivalent('F##', 'G')).toBe(true);
+        expect(areNotesEquivalent('Gbb', 'F')).toBe(true);
+    });
+
+    test('handles case insensitivity', () => {
+        expect(areNotesEquivalent('c#', 'C#')).toBe(true);
+        expect(areNotesEquivalent('Db', 'db')).toBe(true);
+    });
+
+    test('identifies non-equivalent notes', () => {
+        expect(areNotesEquivalent('C#', 'D#')).toBe(false);
+        expect(areNotesEquivalent('F', 'Gb')).toBe(false);
+        expect(areNotesEquivalent('B', 'C')).toBe(false);
+    });
+
+    test('handles complex enharmonic equivalents', () => {
+        expect(areNotesEquivalent('B#', 'C')).toBe(true);
+        expect(areNotesEquivalent('E#', 'F')).toBe(true);
+        expect(areNotesEquivalent('Cb', 'B')).toBe(true);
+        expect(areNotesEquivalent('B##', 'C#')).toBe(true);
+    });
+}); 
