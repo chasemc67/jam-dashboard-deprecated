2d6e66cc9e2707263bb6894f4b336cf832e67629
chase <chase@codegen.com>
2024-12-29 11:11:16 -0800

    Fix so eslint and prettier errors are fixed automatically, and also treated as warnings

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,30 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  webpackFinal: async config => {
+    // Convert ESLint errors to warnings
+    config.module.rules.forEach(rule => {
+      if (
+        rule.use &&
+        rule.use.some(use => use.loader && use.loader.includes('eslint-loader'))
+      ) {
+        rule.use.forEach(use => {
+          if (use.options) {
+            use.options.emitWarning = true;
+            use.options.emitError = false;
+          }
+        });
+      }
+    });
+    return config;
+  },
+};
